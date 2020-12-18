7ba9e64e778ed015d58d156484016d3f890c4fbf
Sunena <K.Sunena@alhasawi.com>
2020-12-18 13:09:07 +0530

    static page design

---
+++ app/src/main/java/com/hasawi/sears/ui/main/view/navigation_drawer_menu/DynamicContentFragment.java
@@ -0,0 +1,16 @@
+package com.hasawi.sears.ui.main.view.navigation_drawer_menu;
+
+import com.hasawi.sears.R;
+import com.hasawi.sears.ui.base.BaseFragment;
+
+public class DynamicContentFragment extends BaseFragment {
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.fragment_dynamic_content;
+    }
+
+    @Override
+    protected void setup() {
+
+    }
+}
